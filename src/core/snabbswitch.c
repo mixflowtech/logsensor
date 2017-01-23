766ddc1e0ac2cded07e153ab10ad94eee0955ad1
nzinfo <limn@coreseek.com>
2017-01-24 03:34:43 +0800

    fail back to share-so module

--- src/core/snabbswitch.c
+++ src/core/snabbswitch.c
@@ -5,7 +5,7 @@
 #include "lua.h"
 #include "lualib.h"
 #include "lauxlib.h"
-
+// #include "luv_pub.h"
 #include <stdint.h>
 
 #if UINTPTR_MAX != UINT64_MAX
@@ -15,6 +15,24 @@
 int argc;
 char** argv;
 
+/*
+static int l_bar( lua_State *L )
+{
+   puts("l_bar called.");
+   return 0;
+}
+
+void luaopen_foo( lua_State *L )
+{
+   static const struct luaL_Reg foo[] = {
+      { "bar", l_bar },
+      { NULL, NULL }
+   };
+   luaL_newlib( L, foo );   // create table containing `bar`
+   lua_setglobal(L, "foo"); // assign that table to global `foo`
+}
+*/
+
 int main(int snabb_argc, char **snabb_argv)
 {
   int n = 0;
@@ -23,6 +41,10 @@ int main(int snabb_argc, char **snabb_argv)
   argv = snabb_argv;
   lua_State* L = luaL_newstate();
   luaL_openlibs(L);
+  // FIXME: make libuv static linked?
+  // n = luaopen_luv(L);
+  // luaopen_foo(L);
+  // printf("load %d ===", n);
   n = luaL_dostring(L, "require \"core.startup\"");
   if(n) {
      printf("%s\n", lua_tostring(L, -1));
